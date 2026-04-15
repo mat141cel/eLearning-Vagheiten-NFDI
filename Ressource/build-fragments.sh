6eebba566fa24361e7cb5b60086390cd7a795347
mat141cel <mattia.celisi@gmail.com>
2026-04-15 14:16:37 +0200

    test

--- Ressource/build-fragments.sh
+++ Ressource/build-fragments.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
-SRC="content"
-OUT="content/fragments"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo SCRIPT_DIR
+SRC="$SCRIPT_DIR/content"
+OUT="$SCRIPT_DIR/content/fragments"
 
 mkdir -p "$OUT"
 
 find "$SRC" -type f -name "*.qmd" | while read -r file; do
   filename=$(basename "$file")
 
-  # skip combined
   if [[ "$filename" == *_combined.qmd ]]; then
     echo "skip: $filename"
     continue
@@ -18,7 +19,6 @@ find "$SRC" -type f -name "*.qmd" | while read -r file; do
   out="$OUT/${filename%.qmd}.inc.qmd"
 
   awk '
-    BEGIN {in_yaml=0}
     /^---[[:space:]]*$/ {in_yaml = 1 - in_yaml; next}
     in_yaml == 0 {print}
   ' "$file" > "$out"
